e296082bf6a2abeb501ddeedc26f7b4dc6a988ce
Gustavo Moya <gmoyatorres@gmail.com>
2017-05-06 17:46:34 -0400

    Changed the navigation flow from tabs to a sidebar menu.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,42 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, MenuController, Nav } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { HomePage } from "../pages/home/home";
+import { AboutPage } from "../pages/about/about";
+import { ContactPage } from "../pages/contact/contact";
 
-import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  @ViewChild(Nav) nav: Nav;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
+  rootPage:any = HomePage;
+  views: Array<Object>;
+
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, public menu: MenuController) {
+    this.initApp();
+
+    this.views = [
+      {title: 'Home', component: HomePage},
+      {title: 'About', component: AboutPage},
+      {title: 'Contact', component: ContactPage}
+    ];    
+  }
+
+  initApp() {
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
     });
+  }
+
+  goTo(view) {
+    this.menu.close();
+    this.nav.setRoot(view.component);
   }
 }
