3b6db89dee567b1a02f8ad0b22fe4da1f692c574
Gael Leonel Abreu <Gael_Abreu@hotmail.com>
2017-05-06 22:07:56 -0400

    Created Dashboard at Home page with Car list, Events list and Notice List

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,27 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  constructor(public navCtrl: NavController) {}
 
-  constructor(public navCtrl: NavController) {
+  CarList = 
+    [
+      {Modelo : "718 Cayman S",            Status : "Listo para entrega"},
+      {Modelo : "911 Carrera Cabriolet",    Status : "En revisión"},
+      {Modelo : "911 GT3",                  Status : "Asignando técnico"},
+      {Modelo : "Panamera 4 Sport Turismo", Status : "En Prueba"},
+      {Modelo : "Macan Turbo", Status : "Area de lavado"},
+    ]
 
-  }
+  NewsList =
+  [
+    {Title : "Nuevo Porsche GT3", Created : "5/Abril/2017"},
+    {Title : "Nueva app móvil", Created : "29/Marzo/2017"},
+  ]
 
+  EventList =
+  [
+    {Title : "Track Race", EventDateTime : "25/Junio/2017 - 3:00 PM"},
+    {Title : "Flexi Móvil BHD", EventDateTime : "30/Junio/2017 - 7:30 PM"},
+    {Title : "Porsche Club", EventDateTime : "7/Julio/2017 - 8:30 PM"}
+  ]
 }
